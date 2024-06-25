7d3a53ff6ac049dadd439932a9ac44865371eb2e
NicolasDeza <nicolasdeza@hotmail.be>
2024-06-25 22:36:13 +0200

    footer et layout

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -5,16 +5,15 @@ export const Layout = () => {
   const year = new Date().getFullYear();
 
   return `
-    <div class="d-flex flex-column min-vh-100">
-      <header>
-      </header>
-      <main class="container mt-5">
-    
-      </main>
-      <footer class="text-center mt-auto">
-        <p>&copy; ${year} - Tous droits réservés</p>
-        
-      </footer>
-    </div>
+   <div class="d-flex flex-column min-vh-100">
+  <header>
+  </header>
+  <main class="container mt-5">
+  </main>
+  <footer class="text-center mt-auto custom-footer">
+    <p>&copy; ${year} - Tous droits réservés</p>
+  </footer>
+</div>
+
     `;
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,26 +7,6 @@
 export const Home = (element) => {
   element.innerHTML = `
     <h1 class="Accueil">Accueil</h1>
-    <div id="homeCarousel" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="./carousel/imgUn.webp" class="d-block w-100" alt="First slide">
-        </div>
-        <div class="carousel-item">
-          <img src="./carousel/imgDeux.webp" class="d-block w-100" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-          <img src="./img/produits/carousel_image_3.png" class="d-block w-100" alt="Third slide">
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
+
   `;
 };
